e9daafc9335bb7c2d4ba2f53aee9fdee00cc4bea
sarveshT7 <sarveshtalekar123@gmail.com>
2022-04-11 17:14:12 +0530

    changing the header name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return(
         <>
-        <h1 className='header'>Movie Company</h1>
+        <h1 className='header'>Movie Company-1</h1>
         </>
     )
 }
